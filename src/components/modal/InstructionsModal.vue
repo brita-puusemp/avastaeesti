bcc4689ba579bc8bff6fd09260dee8edaaeb3c8e
klinkertegel <nmoltmaker@gmail.com>
2025-03-16 23:03:05 +0200

    Niels - nstructions updated

--- src/components/modal/InstructionsModal.vue
+++ src/components/modal/InstructionsModal.vue
@@ -11,9 +11,11 @@
 
       Sa näed pilti ühest Eesti paigast ja sul on piiratud aeg, et ära arvata, kus see asub. Kas sa tead, kus see asub? Suurepärane! , siis klõpsake nüüd nupule „ava kaart“, asetage asukoha kohale nõel ja klõpsake „Kinnita“. Saad näha, kas vastus oli õige, ja sind suunatakse järgmisele pildile.<br><br>
 
-      Ei ole kindel? Vajutage nupule „Võta vihje“ <font-awesome-icon :icon="['far', 'star']" />, et saada vihje.<br><br>
+      Ei ole kindel? Vajutage nupule „Võta vihje“, et saada vihje.<br><br>
 
-      Leidsid meeldiva asukoha ja tahad hiljem rohkem teada saada? Lisa see nupuga „Lisa lemmikute hulka“ lemmikute hulka, et saaksid selle koha kohta hiljem rohkem teada!
+      Leidsid meeldiva asukoha ja tahad hiljem rohkem teada saada? Lisa see nupuga „Lisa lemmikute hulka“ <font-awesome-icon :icon="['far', 'star']" />
+
+      Nautige mängu!
 
     </template>
 
